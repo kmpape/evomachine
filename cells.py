91fbdd6006895fb261c2dace54590251a69682dd
kmpape <kempf.idris@gmail.com>
2023-04-20 12:36:08 +0100

    Finished cell unit tests. Added mother machine. Started image processors.

--- cells.py
+++ cells.py
@@ -21,9 +21,15 @@ class CellPosition:
     def get_trench_id(self) -> int:
         return self._trench_id
 
+    def __str__(self):
+        return f"frame={self._frame_id}, trench={self._trench_id}, (x,y)=({self._x_coord:.2f}, {self._y_coord:.2f})"
+
+    def __eq__(self, other):
+        return ((self._frame_id == other.get_frame_id()) and (self._trench_id == other.get_trench_id()) and
+                (self._x_coord == other.get_x_y_coord()[0]) and (self._y_coord == other.get_x_y_coord()[1]))
+
 
 class CellProperties:
-    # TODO: disp functions
     def __init__(self, area: float, age: float, is_alive: bool,
                  fluo_chan1: float, fluo_chan2: float, fluo_chan3: float):
         self.area: float = area
@@ -33,9 +39,12 @@ class CellProperties:
         self.fluo_chan2: float = fluo_chan2
         self.fluo_chan3: float = fluo_chan3
 
+    def __str__(self):
+        return f"area={self.area}, age={self.age}, alive={self.is_alive}, fluo=({self.fluo_chan1:.2f}, "\
+               f"{self.fluo_chan2:.2f}, {self.fluo_chan3:.2f})"
+
 
 class Cell:
-    # TODO: disp functions, comparisons
     def __init__(self, cell_id: int, mask_id: int, position: 'CellPosition', properties: 'CellProperties'):
         self._cell_id: int = cell_id
         self._mask_id: int = mask_id
@@ -67,6 +76,12 @@ class Cell:
 
     def get_position(self) -> 'CellPosition':
         return self._position
+
+    def __str__(self):
+        return f"Cell ID={self._cell_id}, mask ID={self._mask_id}"
+
+    def __eq__(self, other):
+        return self._cell_id == other.get_cell_id()
 
 
 class CellFactory:
@@ -113,36 +128,15 @@ class CellFactory:
         position: 'CellPosition' = CellPosition(frame_id=self._frame_id, trench_id=self._trench_id,
                                                 x_coord=self._x_coord, y_coord=self._y_coord)
         properties: 'CellProperties' = CellProperties(area=self._area, age=self._age, is_alive=self._is_alive,
-                                                      fluo_chan1=self._fluo_chan1, fluo_chan2=self._fluo_chan3,
+                                                      fluo_chan1=self._fluo_chan1, fluo_chan2=self._fluo_chan2,
                                                       fluo_chan3=self._fluo_chan3)
         cell = Cell(cell_id=self._cell_id, mask_id=self._mask_id, position=position, properties=properties)
         self._cell_id += 1
         return cell
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+class MotherMachine:
+    def __init__(self):
+        # TODO: probably need to index this via frame_id->trench_id
+        self.cells: List['Cell'] = []
+        self.cell_factory: 'CellFactory' = CellFactory()
--- imageprocessor.py
+++ imageprocessor.py
@@ -1,41 +1,54 @@
 import logging
 import numpy as np
 from time import perf_counter
+from typing import List
 
 from cellpose_omni import models
 
 import config
+from cells import MotherMachine
 
 logger = logging.getLogger(__name__)
 
 
 class ImageProcessor(object):
-    def __init__(self, debug_mode: bool = config.DEBUG_MODE):
-        self.debug_mode: bool = debug_mode
+    def __init__(self, frame_rows: int, frame_cols: int, num_frames: int, dtype: 'np.dtype'):
+        self.new_frames: List[np.array] = [np.zeros((frame_rows, frame_cols), dtype=dtype)
+                                           for _ in range(0, num_frames)]
+        self.old_frames: List[np.array] = [np.zeros((frame_rows, frame_cols), dtype=dtype)
+                                           for _ in range(0, num_frames)]
 
-    def process_image(self, new_image: np.ndarray, old_image: np.ndarray) -> np.ndarray:
-        if self.debug_mode:
+    def process_image(self, new_frame: np.ndarray, frame_id: int, mother_machine: 'MotherMachine'):
+        self._update_frames(new_frame, frame_id)
+        if config.DEBUG_MODE:
             time_start = perf_counter()
-        masks, flows, styles = self.model.eval(x=new_image, batch_size=batch_size, channels=[0, 0], rescale=None,
-                                               mask_threshold=-1, transparency=True, flow_threshold=0., omni=omni,
-                                               resample=True, verbose=0)
-        if self.debug_mode:
+        self._process_image(mother_machine)
+        if config.DEBUG_MODE:
             elapsed: float = perf_counter() - time_start
+            logger.debug(f"Processed image in {elapsed:.9f}s")
+
+    def _update_frames(self, new_frame: np.ndarray, frame_id: int):
+        self.old_frames[frame_id] = self.new_frames[frame_id]
+        self.new_frames[frame_id] = new_frame
+
+    def _process_image(self, mother_machine: 'MotherMachine'):
+        raise NotImplementedError()
 
 
-class OmniposeProcessor(ImageProcessor):
-    def __init__(self, debug_mode: bool = config.DEBUG_MODE, model_path: str = config.OMNIPOSE_MODEL_PATH):
-        super().__init__(debug_mode=debug_mode)
-        self.model_path: str = model_path
-        self.model: 'models.CellposeModel' = models.CellposeModel(gpu=True, pretrained_model=self.model_path, omni=True,
-                                                                  concatenation=True)
-        logger.debug("Using CellposeModel={}".format(self.model_path))
-
-    def process_image(self, new_image: np.ndarray):
-        if self.debug_mode:
-            time_start = perf_counter()
-        masks, flows, styles = self.model.eval(x=new_image, batch_size=config.OMNIPOSE_BATCH_SIZE, channels=[0, 0],
-                                               rescale=None, mask_threshold=-1, transparency=True, flow_threshold=0., omni=omni,
-                                               resample=True, verbose=0)
-        if self.debug_mode:
-            elapsed: float = perf_counter() - time_start
+# class OmniposeProcessor(ImageProcessor):
+#     def __init__(self, model_path: str = config.OMNIPOSE_MODEL_PATH):
+#         super().__init__(debug_mode=debug_mode)
+#         self.model_path: str = model_path
+#         self.model: 'models.CellposeModel' = models.CellposeModel(gpu=True, pretrained_model=self.model_path, omni=True,
+#                                                                   concatenation=True)
+#         logger.debug("Using CellposeModel={}".format(self.model_path))
+#
+#     def process_image(self, new_image: np.ndarray):
+#         if self.debug_mode:
+#             time_start = perf_counter()
+#         # TODO
+#         masks, flows, styles = self.model.eval(x=new_image, batch_size=config.OMNIPOSE_BATCH_SIZE, channels=[0, 0],
+#                                                rescale=None, mask_threshold=-1, transparency=True, flow_threshold=0., omni=omni,
+#                                                resample=True, verbose=0)
+#         if self.debug_mode:
+#             elapsed: float = perf_counter() - time_start
