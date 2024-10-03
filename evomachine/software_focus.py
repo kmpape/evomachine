fe47beff7c1a8e247b3441466f05a066d6e60a71
kmpape <kempf.idris@gmail.com>
2024-10-03 13:41:13 +0100

    Changes to software focus

--- evomachine/software_focus.py
+++ evomachine/software_focus.py
@@ -2,23 +2,54 @@ import cv2
 import numpy as np
 from typing import Optional
 
+from delta.utils import CroppingBox
+
 from evomachine.exceptions import ConfigError, ErrorCode
 from evomachine.evotypes import FocusAlgorithmType
 
 
-DEFAULT_SQUARED_GRAD_THRESHOLD = 0.1
+DEFAULT_SQUARED_GRAD_THRESHOLD = 0
 
 def get_focus_score_is_good(focus_curve: np.array) -> bool:  # noqa
     return True
 
 
-def get_focus_score(img: np.array, algorithm: FocusAlgorithmType, threshold: Optional[float] = None) -> float:
+def get_roi_focus_score(
+        img: np.array,
+        algorithm: FocusAlgorithmType,
+        boxes: list[CroppingBox],
+        threshold: Optional[float] = None,
+        rowshift: int = 25,
+        colshift: int = 50,
+        normalise_score: bool = False,
+) -> float:
+    total_score = 0
+    for box in boxes:
+        if algorithm == FocusAlgorithmType.LAPLACIAN_VAR:
+            total_score += get_focus_score_laplacian_var(img=box.crop(img))
+        elif algorithm == FocusAlgorithmType.SQUARED_GRAD_AVG:
+            total_score += get_focus_score_squared_gradient(img=box.crop(img), threshold=threshold)
+        elif algorithm == FocusAlgorithmType.STEEL:
+            total_score += get_focus_score_steel(img=box.crop(img), rowshift=rowshift, colshift=colshift, normalise=normalise_score)
+        else:
+            raise ConfigError(f"get_focus_score: Unknown focus algorithm: {algorithm}.", ErrorCode.ERROR_FOCUS_CONFIG)
+    return total_score
+
+
+def get_focus_score(
+        img: np.array,
+        algorithm: FocusAlgorithmType,
+        threshold: Optional[float] = None,
+        rowshift: int = 25,
+        colshift: int = 50,
+        normalise_score: bool = False,
+) -> float:
     if algorithm == FocusAlgorithmType.LAPLACIAN_VAR:
         return get_focus_score_laplacian_var(img=img)
     elif algorithm == FocusAlgorithmType.SQUARED_GRAD_AVG:
         return get_focus_score_squared_gradient(img=img, threshold=threshold)
     elif algorithm == FocusAlgorithmType.STEEL:
-        return get_focus_score_steel(img=img)
+        return get_focus_score_steel(img=img, rowshift=rowshift, colshift=colshift, normalise=normalise_score)
     else:
         raise ConfigError(f"get_focus_score: Unknown focus algorithm: {algorithm}.", ErrorCode.ERROR_FOCUS_CONFIG)
 
@@ -35,13 +66,17 @@ def get_focus_score_squared_gradient(img: np.array, threshold: Optional[float] =
     return (tmp**2).mean()
 
 
-def get_focus_score_steel(img: np.array, rowshift: int = 25, colshift: int = 50):
+def get_focus_score_steel(img: np.array, rowshift: int, colshift: int, normalise=False):
+    img_float = img.astype(float)
     img_row = np.multiply(
-        np.abs(img - np.roll(img, -rowshift, axis=0)),
-        np.abs(img - np.roll(img, +rowshift, axis=0)),
+        np.abs(img_float - np.roll(img_float, -rowshift, axis=0)),
+        np.abs(img_float - np.roll(img_float, +rowshift, axis=0)),
     )
     img_col = np.multiply(
-        np.abs(img - np.roll(img, -colshift, axis=1)),
-        np.abs(img - np.roll(img, +colshift, axis=1)),
+        np.abs(img_float - np.roll(img_float, -colshift, axis=1)),
+        np.abs(img_float - np.roll(img_float, +colshift, axis=1)),
     )
-    return img_row.sum() + img_col.sum()
+    if normalise:
+        return (img_row.sum() + img_col.sum()) / float(2 * img.shape[0] * img.shape[1])
+    else:
+        return img_row.sum() + img_col.sum()
