a5dc0e544b76780fdad3aecb6e2decad190f8ed0
Robin Henry <robin.henry012@gmail.com>
2026-06-15 22:35:49 +0100

    remove old sys.path paths to other sibling repos, now covered in pyproject.toml

--- evomachine/__init__.py
+++ evomachine/__init__.py
@@ -1,19 +1,6 @@
-import sys
 import os
 import logging
-from pathlib import Path
 import matplotlib.pyplot as plt
-
-WORKSPACE_ROOT = Path(__file__).resolve().parents[2]
-
-delta_path = str(WORKSPACE_ROOT / "de-lta-rt")
-sys.path.append(delta_path)
-
-asitiger_path = str(WORKSPACE_ROOT / "asitiger")
-sys.path.append(asitiger_path)
-
-sync_board_path = str(WORKSPACE_ROOT / "sync_board")
-sys.path.append(sync_board_path)
 
 # Set TF logging level:
 # 0 = all messages are logged (default behavior)
